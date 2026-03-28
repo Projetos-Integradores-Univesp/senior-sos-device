1e7f2889dc9c9d10974718b231c6dec9109a42a9
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2026-03-27 22:51:43 -0300

    Add router create_account.

--- backend/auth_routes.py
+++ backend/auth_routes.py
@@ -1,11 +1,30 @@
 from fastapi import APIRouter
-
+from sqlalchemy.orm import sessionmaker
+from backend.models import db, User
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_router.get("/")
-async def authentication():
+async def home():
     """Rota padrão para autenticação de usuários."""
 
     return {"Você acessou a rota autenticação."}
+
+
+@auth_router.post("/create_account")
+async def create_account(username: str, password: str):
+    # Criando uma seção no DB
+    Session = sessionmaker(bind=db)
+    session = Session()
+
+    # fazendo buscas no DB pela existência do nome de usuário
+    username_exists_in_db = session.query(User).filter(User.username == username).first()
+
+    if not username_exists_in_db:
+        new_user = User(username, password)
+        session.add(new_user)
+        session.commit()
+        return {"create_acount": True}
+    else:
+        return {"create_acount": False}
