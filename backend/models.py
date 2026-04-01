15a4b283b186a0f4b2d2b8f6e9d22c5465e7ab34
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2026-03-31 22:39:53 -0300

    Refactor.

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy import Integer, String, DateTime, Boolean
-from sqlalchemy import func
 from sqlalchemy.orm import declarative_base
 from sqlalchemy_utils.types import ChoiceType
+from datetime import datetime
 
 # Conexão ado banco de dados
 # Passar link do banco de dados quando em produção aqui
@@ -21,7 +21,7 @@ class User(Base):
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     username = Column("username", String(128), unique=True, nullable=False)
     password_hash = Column("password_hash", String(256), nullable=False)
-    created_at = Column("created_at", DateTime, default=func.now())
+    created_at = Column("created_at", DateTime, default=datetime.now())
     status = Column("status", Boolean, default=True)
 
     def __init__(self, username, password_hash, status=True):
@@ -37,7 +37,7 @@ class Session(Base):
 
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     user = Column("user", ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    login_time = Column("login_time", DateTime, default=func.now())
+    login_time = Column("login_time", DateTime, default=datetime.now())
     logout_time = Column("logout_time", DateTime)
 
     def __init__(self, user: User):
@@ -52,7 +52,7 @@ class Device(Base):
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     admin = Column("admin", ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     nickname = Column("nickname", String(128), unique=True, nullable=False)
-    created_at = Column("created_at", DateTime, default=func.now())
+    created_at = Column("created_at", DateTime, default=datetime.now())
 
     def __init__(self, admin: User, nickname):
         self.admin = admin
@@ -69,7 +69,7 @@ class Event(Base):
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     device = Column("device", ForeignKey("devices.id", ondelete="CASCADE"), nullable=False)
     type = Column("type", ChoiceType(EVENTS_TYPES))
-    time = Column("created_at", DateTime, default=func.now())
+    time = Column("created_at", DateTime, default=datetime.now())
 
     def __init__(self, device: Device, type="BUTTON PRESSED"):
         self.device = device
