e588314dd7819c99b578d6593b9ee05c35b9ed6f
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2026-04-08 23:06:26 -0300

    Finished function test for auth/refresh.

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -2,3 +2,13 @@ import requests
 
 url = "http://127.0.0.1:8000/auth/refresh"
 response = requests.get(url)
+
+refresh_token = "colar refresh_token aqui..."
+
+headers = {
+    "Authorization": f"Bearer {refresh_token}",
+    "Content-Type": "application/json",
+}
+
+response = requests.get(url, headers=headers)
+print(response.json())
