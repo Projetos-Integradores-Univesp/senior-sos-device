e37195b23f3925d3a64376e4f67ff76cfbf458fc
Tiago D. Barbieri <tiagodbarbieri@hotmail.com>
2026-04-11 18:14:28 -0300

    Added user update account router.

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 from backend.models import User
-from backend.utils import get_db_session
+from backend.utils import get_db_session, token_validation
 from backend.utils import generate_hash
 from backend.schemas import UserCredentials
 
@@ -24,3 +25,44 @@ async def create_account(user_credentials: UserCredentials, session: Session = D
         return {"detail": {"message": "User successfully registered.", "created_acount": True}}
     else:
         raise HTTPException(status_code=400, detail={"message": "User already registered.", "created_acount": False})
+
+
+@users_router.put("/me")
+async def update_account(
+    new_credentials: UserCredentials, session: Session = Depends(get_db_session), user: User = Depends(token_validation)
+):
+    """
+    Rota para atualização do "username" e "password" do usuário.
+    Necessário enviar novo "username" e "password".
+    Necessário enviar no header dessa requisição:
+
+        headers = {
+            "Authorization": "Bearer SEU_ACCESS_TOKEN_AQUI",
+            "Content-Type": "application/json"
+        }
+
+    Em "SEU_ACCESS_TOKEN_AQUI" substituir pelo "access_token" do usuário.
+    """
+
+    # fazendo buscas no DB pela existência do nome de usuário
+    username_exists_in_db = session.query(User).filter(User.username == new_credentials.username).first()
+
+    if not username_exists_in_db:
+        # Construindo a instrução de atualização
+        statement = (
+            update(User)
+            .where(User.id == user.id)
+            .values(username=new_credentials.username, password_hash=generate_hash(new_credentials.password))
+        )
+
+        session.execute(statement)
+        session.commit()
+
+        return {"detail": {"message": "User updated successfully.", "updated": True}}
+    else:
+        raise HTTPException(status_code=400, detail={"message": "Username already registered.", "updated": False})
+
+
+@users_router.delete("/me")
+async def delete_account():
+    pass
